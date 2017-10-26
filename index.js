c31c43e92bae556f664b80c21385227e23322eba
BDafflon <trysac@gmail.com>
2017-10-26 09:57:29 +0200

    session

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var port = process.env.PORT || 3000;
 const express = require('express'),
+	  session = require('express-session'),
+	  cookieParser = require('cookie-parser'),
       app     = express(),
       server  = require('http').createServer(app).listen(port),
       io      = require('socket.io')(server),
@@ -9,7 +11,19 @@ const express = require('express'),
 // Dossier static
 app.use(express.static(__dirname + '/public'));
  
+// session
+app.use(cookieParser());
+app.use(session({secret: 'openCPSCookies'}));
+
 // Routes
+ 
 app.get('/', function(req, res) {
-   res.sendFile(path.join(__dirname + '/public/index.html'));
+	 if(req.session.login){
+		 
+		res.sendFile(path.join(__dirname + '/public/index.html'));
+	 }
+	 else{
+		 req.session.page_views = 1;
+		 res.sendFile(path.join(__dirname + '/public/index2.html'));
+	 }
 })
